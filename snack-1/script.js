b98087db6bf74cfbea8187d26fdece62d26a3e81
francescapetra <francescalaurapetra@gmail.com>
2021-03-02 11:16:40 +0100

    snack-1

---
+++ snack-1/script.js
@@ -0,0 +1,13 @@
+// L’utente inserisce due numeri in successione, con due prompt.
+var primoNumero = parseInt(prompt("inserisci un numero"));
+// console.log (primoNumero);
+
+var secondoNumero = parseInt(prompt("inserisci un nuovo numero"));
+// console.log (secondoNumero);
+// Il software stampa il maggiore.
+
+if (primoNumero > secondoNumero) {
+ console.log(primoNumero);
+}else if (secondoNumero > primoNumero) {
+ console.log(secondoNumero);
+}
