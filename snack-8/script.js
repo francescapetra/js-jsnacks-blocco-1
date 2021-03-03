afc4483622d8150e76ae5cf2e405642688ff5ef8
francescapetra <francescalaurapetra@gmail.com>
2021-03-03 13:11:52 +0100

    snack-8 = 10

--- snack-8/script.js
+++ snack-8/script.js
@@ -7,7 +7,7 @@ numero = prompt("inserisci numero");
 somma = 0;
 
 
-for (var i = 1; i < numero.length; i++) {
+for (var i = 0; i < numero.length; i++) {
   somma += parseInt(numero[i]);
 }
 console.log(somma);
