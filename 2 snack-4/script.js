4c77bb850bbb969547bf8bcae5f61d0e199c49d8
francescapetra <francescalaurapetra@gmail.com>
2021-03-04 14:51:54 +0100

    2-snack-4

--- 2 snack-4/script.js
+++ 2 snack-4/script.js
@@ -1,13 +1,13 @@
 // crea un array di numeri interi e fai la somma di quelli in posizione dispari
 
-var numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+var numeri = [1, 25, 31, 48, 55, 6, 7, 8, 9 ];
 
 var somma = 0;
 
 for (var i = 0; i <numeri.length; i++) {
-
-  if (numeri[i] % 2 ==1) {
-    console.log(numeri[i]);
+//console.log(numeri[i]);
+  if (i % 2 ==1) {
+    //console.log(numeri[i]);
     somma += numeri[i];
   }
 }
