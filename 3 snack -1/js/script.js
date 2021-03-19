0ed14f6308eafcff892b876987fcb31a9c5fac86
francescapetra <francescalaurapetra@gmail.com>
2021-03-19 17:46:46 +0100

    ottimizzati

--- 3 snack -1/js/script.js
+++ 3 snack -1/js/script.js
@@ -1,4 +1,6 @@
-// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
 // creo array oggetti
 var bike = [
--- 3 snack -2/js/script.js
+++ 3 snack -2/js/script.js
@@ -19,38 +19,36 @@ var squadre = [
     falli : 0,
   },
 ];
+console.log(squadre);
 // 2 step:
 // Generare numeri random al posto degli 0 nelle proprietà:
 // Punti fatti e falli subiti.
 // creo una variabile fuori dove prendo l'indice dell'objt
 
 // var squadre[i];
-for (var key in squadre) {
-  console.log([key]);
-  console.log(squadre[key].punti, squadre[key].falli)
-
-  if (squadre[key].punti == 0) {
-    squadre[key].punti = randomGeneratore(1, 10);
-  }
-  if (squadre[key].falli == 0) {
-    squadre[key].falli = randomGeneratore(1, 10);
-  }
-}
-for (var i = 0; i < squadre.length; i++) {
-  console.log(squadre);
-}
-
-
 function randomGeneratore(min, max){
   var numeriCpu = Math.floor(Math.random() * max) + 1;
   return numeriCpu;
 }
+
+for (var i = 0; i < squadre.length; i++) {
+
+  squadre[i].punti = randomGeneratore(0, 10);
+  squadre[i].falli = randomGeneratore(0, 10);
+  console.log(squadre[i].punti);
+}
+
 // 3 step:
 // Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 //
 var nuovoArray = [];
 
-for (var key in squadre) {
-nuovoArray.push(squadre[key].nome, squadre[key].falli)
+for (var i = 0; i < squadre.length; i++) {
+
+nuovoArray.push({
+  nome: squadre[i].nome,
+  falli: squadre[i].falli
+
+});
 }
 console.log(nuovoArray);
